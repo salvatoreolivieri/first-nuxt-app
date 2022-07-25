e63f7a2ce31f1450a778fe1d4254c18913f9fd53
salvatoreolivieri <Salvatore.olivieri07@gmail.com>
2022-07-25 19:46:25 +0200

    First approach to view, page and routing.

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,19 @@
 <template>
-  <Tutorial/>
+
+  <div>
+
+    <nuxt-logo />
+
+    <h1>HomePage</h1>
+    <a href="/products">Go To Products</a>
+  </div>
+
 </template>
 
 <script>
+import NuxtLogo from '../components/NuxtLogo.vue'
 export default {
+  components: { NuxtLogo },
   name: 'IndexPage'
 }
 </script>
---
+++ pages/products/index.vue
@@ -0,0 +1,10 @@
+<template>
+
+  <div>
+    <h1>Products Page</h1>
+
+    <a href="/products/1">Product one</a>
+    <a href="/products/2">Product two</a>
+  </div>
+
+</template>
